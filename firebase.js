3c4f6716a3d4dbc6791beac0c198fd50ee6b4ab8
Josh Claxton <jrccode@outlook.com>
2023-09-24 22:43:31 -0500

    header and firebase hosting setup

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBr0o1BHhQxJTSKrPo0eRGxvB11Lkl5ztc',
+  authDomain: 'amazingg-2de0a.firebaseapp.com',
+  projectId: 'amazingg-2de0a',
+  storageBucket: 'amazingg-2de0a.appspot.com',
+  messagingSenderId: '574829739051',
+  appId: '1:574829739051:web:d126000c6bcbeaf4723e6e',
+  measurementId: 'G-CRLKRSQ7BT',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app);
