07d8462d95a1a4e7d4b52caa83fcba421cabbca4
Josh Claxton <jrccode@outlook.com>
2023-10-02 11:39:35 -0500

    carousel and introduction, nav styling and coloring

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,33 +1,63 @@
+'use client'
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import './components.css'
+import { BsFillCartFill, BsFillPersonFill } from 'react-icons/bs'
 
 const Navbar = () => {
+  const [search, setSearch] = useState('Search...')
+
+  const handleInputFocus = () => {
+    if (search === 'Search...') {
+      setSearch('')
+    }
+  }
+
+  const handleInputAbort = () => {
+    if (search === '') {
+      setSearch('Search...')
+    }
+  }
   return (
-    <nav>
-      <Image
-        src='/amazingg_logo_transparent.png'
-        alt='Amazing G'
-        className='title'
-        width={256}
-        height={160}
-        style={{ position: 'absolute', marginTop: 1 }}
-      />
-      <div></div>
+    <>
+      <nav>
+        <Image
+          src='/logo_v1_transparent.png'
+          alt='Amazing G'
+          className='title'
+          width={500}
+          height={150}
+        />
+
+        <div className='search-container'>
+          <input
+            type='text'
+            name='search'
+            value={search}
+            defaultValue={search}
+            className='search-bar'
+            onFocus={handleInputFocus}
+            onBlur={handleInputAbort}
+          />
+        </div>
+        <div className='icons'>
+          <BsFillCartFill className='icon' />
+          <BsFillPersonFill className='icon' />
+        </div>
+      </nav>
       <div className='links-container'>
         <ul className='links-list'>
           <li>Home</li>
-          <li></li>
-        </ul>
-        <ul className='links-list-2'>
-          <li>Search</li>
+          <li>Women</li>
+          <li>Men</li>
+          <li>Jewelry</li>
+          <li>Sports</li>
+          <li>Tech</li>
+          <li style={{ color: 'green', fontWeight: 'bold' }}>GREEN</li>
         </ul>
       </div>
-      <div className='icons'>
-        <span>S</span>
-        <span>L</span>
-      </div>
-    </nav>
+    </>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,8 +1,32 @@
 import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ProductCarousel from './components/ProductCarousel'
+import './styles.css'
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <ProductCarousel />
+      <main className='main-home'>
+        <div className='gloria-card'>
+          <h2>About Us</h2>
+          <p>
+            Amazing G is more than a store for us. It is more than an online
+            shop, it is more than a company. Amazing G is a gathering of those
+            who want to help others. We strive to be able to help those who want
+            to make something of themselves.{' '}
+          </p>
+          <p>
+            With all that is going on in the world, we at Amazing G believe that
+            we must all look out for each other. That is why we are here to
+            help. Gloria Apolinario is a verified distributor of Alibaba goods
+            and products, and is using this position to pay it forward as much
+            as possible. The prices you see are solid prices Gloria has decided
+            on, keeping it at the lowest possible margin for ourselves and the
+            highest profit for our customers.
+          </p>
+        </div>
+      </main>
     </main>
   )
 }
